23b07b5ad22c6ecb922b7eff0bdcd73f58312d39
Владислав Горохов <gbg.0101@mail.ru>
2019-11-06 18:21:18 +0600

    first commit

---
+++ SiteNews/Areas/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using SiteNews.DataContext;
+using SiteNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteNews.Areas.AdminPanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: AdminPanel/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string text, string imageUrl)
+        {
+            using(var context = new NewsContext())
+            {
+                context.News.Add(new Models.News { Text = text, ImagePath = imageUrl });
+                context.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ SiteNews/Areas/News/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using SiteNews.DataContext;
+using SiteNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteNews.Areas.News.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: News/Home
+        public ActionResult Index()
+        {
+            using(var context = new NewsContext())
+            {
+                ViewBag.News = context.News.ToList(); 
+                return View();
+            }
+                
+        }
+    }
+}
